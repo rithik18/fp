e690c26dac737cb2578a817879f1bf65fa5a464e
rithik18 <rithikharendarm@gmail.com>
2024-09-30 16:38:06 +0530

    frontend

--- fullstack/frontend/src/App.tsx
+++ fullstack/frontend/src/App.tsx
@@ -1,11 +1,14 @@
-
+import { BrowserRouter,Routes,Route} from "react-router-dom";
+import Login from './pages/Login'
 function App() {
 
 
   return (
-    <>
-      <p>Hello</p>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login/>}></Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
